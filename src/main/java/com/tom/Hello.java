0dfb49b99015a292c4f0bc7b6f4f57dcf12a2d13
ShangRuey <tom830522@gmail.com>
2023-06-05 17:42:54 +0800

    6/5 Java and Kotlin 建構式

--- src/main/java/com/tom/Hello.java
+++ src/main/java/com/tom/Hello.java
@@ -5,10 +5,8 @@ public class Hello
     public static void main(String[] args)
     {
 //        System.out.println("Hello world");
-        Person p = new Person();
+        Person p = new Person( "Tom",66.5f,1.7f);
         p.hello();
-        p.weight=66.5f;
-        p.height = 1.7f;
         System.out.println(p.bmi());
     }
 }
--- src/main/kotlin/com/tom/Hello.kt
+++ src/main/kotlin/com/tom/Hello.kt
@@ -5,12 +5,17 @@ fun main()
 //    println("Hello World");
 //    Human().hello()
 //    h.hello()
-    val h = Human( weight = 66.5f, height = 1.7f)
+    val h = Human( 66.5f,1.7f)
     println(h.bmi())
+    h.hello()
 }
 
-class Human(var weight: Float , var height : Float)
+class Human(var weight: Float , var height : Float,var name:String = "")
 {
+    init{
+        println("test $weight")
+    }
+//    constructor(name: String, weight: Float, height: Float) :   this(weight,height)
     fun bmi() :Float
     {
         val bmi = weight / (height*height)
